5c3f704737bcf82a4556656f708038600874ce2b
Shivam Singh <shivamss17.98@gmail.com>
2026-06-05 15:29:32 +0100

    Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './layout/nav/nav.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, NavComponent, FooterComponent],
+  template: `
+    <app-nav />
+    <main class="main-content">
+      <router-outlet />
+    </main>
+    <app-footer />
+  `,
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+      }
+      .main-content {
+        flex: 1;
+      }
+    `,
+  ],
 })
-export class AppComponent {
-  title = 'shivam-personal';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'projects',
+    loadComponent: () =>
+      import('./pages/projects/projects.component').then(
+        (m) => m.ProjectsComponent,
+      ),
+  },
+  {
+    path: 'projects/:slug',
+    loadComponent: () =>
+      import('./pages/project-detail/project-detail.component').then(
+        (m) => m.ProjectDetailComponent,
+      ),
+  },
+  {
+    path: 'blog',
+    loadComponent: () =>
+      import('./pages/blog/blog.component').then((m) => m.BlogComponent),
+  },
+  {
+    path: 'blog/:slug',
+    loadComponent: () =>
+      import('./pages/blog-detail/blog-detail.component').then(
+        (m) => m.BlogDetailComponent,
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
